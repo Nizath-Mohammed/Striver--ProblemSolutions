0e2791da51847c4415c8748eba4b358273ee64c4
Nizath-mohammed <Kit27.ad36@gmail.com>
2025-12-16 15:22:26 +0530

    solution to the inorder traversal was added

--- Optimal/Binary-Tree/postOrdertraversel.java
+++ Optimal/Binary-Tree/postOrdertraversel.java
@@ -1,14 +1,14 @@
-// class Solution {
-//     public void postOrder(TreeNode root , List<Integer> ans){
-//         if(root!=null){
-//             postOrder(root.left , ans);
-//             postOrder(root.right , ans);
-//             ans.add(root.val);
-//         }
-//     }
-//     ublic List<Integer> postorderTraversal(TreeNode root) {
-//         List<Integer> ans = new ArrayList<>();
-//         postOrder(root , ans);
-//         return ans;
-//     }
-// }
+class Solution {
+    public void postOrder(TreeNode root , List<Integer> ans){
+        if(root!=null){
+            postOrder(root.left , ans);
+            postOrder(root.right , ans);
+            ans.add(root.val);
+        }
+    }
+    ublic List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        postOrder(root , ans);
+        return ans;
+    }
+}
